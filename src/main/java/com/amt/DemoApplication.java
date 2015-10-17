de42c153af47a52d7e68eac57dc612e41e94bf1d
mmteyeb <teyeb.med.malek@gmail.com>
2015-10-17 11:42:02 +0200

    initial commit

---
+++ src/main/java/com/amt/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.amt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+
+}
+
