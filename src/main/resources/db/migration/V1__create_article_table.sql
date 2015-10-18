d7431845837f7ad4bc675a4b1232faef0f677e4d
mmteyeb <teyeb.med.malek@gmail.com>
2015-10-18 22:29:35 +0200

    Commit

--- src/main/resources/db/migration/V1__create_article_table.sql
+++ src/main/resources/db/migration/V1__create_article_table.sql
@@ -7,6 +7,6 @@ CREATE TABLE article (
   title           VARCHAR(100)  NOT NULL,
   author          VARCHAR(40)   NOT NULL,
   content         VARCHAR(1000) NOT NULL,
-  submission_date DATE,
+  submission_date TIMESTAMP              DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 );
