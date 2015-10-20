73897c046aadc2be95fd3d874e6b18efba40efdd
mmteyeb <teyeb.med.malek@gmail.com>
2015-10-20 23:42:54 +0200

    Main page refactoring

--- src/main/webapp/javascript/controllers/IndexController.js
+++ src/main/webapp/javascript/controllers/IndexController.js
@@ -9,6 +9,20 @@ app.config(function ($translateProvider) {
     $translateProvider.preferredLanguage('arAR');
 });
 
+app.directive('cTopBar', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'component/top-bar.html'
+    };
+});
+
+app.directive('cMainMenu', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'component/main-menu.html'
+    };
+});
+
 app.directive('cIndex', function () {
     return {
         restrict: 'E',
@@ -61,6 +75,38 @@ app.controller('ArticlesCtrl', ['$scope', 'ArticlesService', 'SlidesService', '$
                     startheight: 500,
                     hideThumbs: 10
                 });
+        });
+        jQuery(document).ready(function(){
+            jQuery("body").fitVids();
+        });
+        jQuery(document).ready(function ($) {
+            var rsi = $('#slider-in-laptop').royalSlider({
+                autoHeight: false,
+                arrowsNav: false,
+                fadeinLoadedSlide: false,
+                controlNavigationSpacing: 0,
+                controlNavigation: 'bullets',
+                imageScaleMode: 'fill',
+                imageAlignCenter: true,
+                loop: false,
+                loopRewind: false,
+                numImagesToPreload: 6,
+                keyboardNavEnabled: true,
+                autoScaleSlider: true,
+                autoScaleSliderWidth: 486,
+                autoScaleSliderHeight: 315,
+
+                /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
+                imgWidth: 792,
+                imgHeight: 479
+
+            }).data('royalSlider');
+            $('#slider-next').click(function () {
+                rsi.next();
+            });
+            $('#slider-prev').click(function () {
+                rsi.prev();
+            });
         });
     });
 }]);
