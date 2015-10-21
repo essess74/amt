d747f7d514e0a023818d9d3e8bd531c11a59979a
mmteyeb <teyeb.med.malek@gmail.com>
2015-10-21 23:33:51 +0200

    First page slide

--- src/main/webapp/javascript/controllers/IndexController.js
+++ src/main/webapp/javascript/controllers/IndexController.js
@@ -4,47 +4,4 @@ app.controller('IndexCtrl', ['$scope', 'ArticlesService', 'SlidesService', '$sce
     $scope.trustSrc = function (url) {
         return $sce.trustAsResourceUrl(url);
     }
-    $timeout(function () {
-        jQuery(document).ready(function () {
-            jQuery('.fullwidthbanner').revolution(
-                {
-                    delay: 7000,
-                    startwidth: 1080,
-                    startheight: 500,
-                    hideThumbs: 10
-                });
-        });
-        jQuery(document).ready(function () {
-            jQuery("body").fitVids();
-        });
-        jQuery(document).ready(function ($) {
-            var rsi = $('#slider-in-laptop').royalSlider({
-                autoHeight: false,
-                arrowsNav: false,
-                fadeinLoadedSlide: false,
-                controlNavigationSpacing: 0,
-                controlNavigation: 'bullets',
-                imageScaleMode: 'fill',
-                imageAlignCenter: true,
-                loop: false,
-                loopRewind: false,
-                numImagesToPreload: 6,
-                keyboardNavEnabled: true,
-                autoScaleSlider: true,
-                autoScaleSliderWidth: 486,
-                autoScaleSliderHeight: 315,
-
-                /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
-                imgWidth: 792,
-                imgHeight: 479
-
-            }).data('royalSlider');
-            $('#slider-next').click(function () {
-                rsi.next();
-            });
-            $('#slider-prev').click(function () {
-                rsi.prev();
-            });
-        });
-    });
 }]);
--- src/main/webapp/javascript/directives/Directives.js
+++ src/main/webapp/javascript/directives/Directives.js
@@ -32,3 +32,51 @@ app.directive('cCopyright', function () {
         templateUrl: 'component/copyright.html'
     };
 });
+
+app.directive('lastElementInRepeat', ['$timeout', function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                if (scope.$last === true) {
+                    $timeout(function(){
+                        jQuery('.fullwidthbanner').revolution(
+                            {
+                                delay: 7000,
+                                startwidth: 1080,
+                                startheight: 500,
+                                hideThumbs: 10
+                            });
+                        jQuery("body").fitVids();
+                        var rsi = $('#slider-in-laptop').royalSlider({
+                            autoHeight: false,
+                            arrowsNav: false,
+                            fadeinLoadedSlide: false,
+                            controlNavigationSpacing: 0,
+                            controlNavigation: 'bullets',
+                            imageScaleMode: 'fill',
+                            imageAlignCenter: true,
+                            loop: false,
+                            loopRewind: false,
+                            numImagesToPreload: 6,
+                            keyboardNavEnabled: true,
+                            autoScaleSlider: true,
+                            autoScaleSliderWidth: 486,
+                            autoScaleSliderHeight: 315,
+
+                            imgWidth: 792,
+                            imgHeight: 479
+
+                        }).data('royalSlider');
+                        $('#slider-next').click(function () {
+                            rsi.next();
+                        });
+                        $('#slider-prev').click(function () {
+                            rsi.prev();
+                        });
+                    });
+                }
+
+            }
+        }
+    }]
+);
